122f930bdb266f1788ca7d5eb593024288b58f56
Emilio Monteiro <monteiroemilio@hotmail.com>
2023-07-19 16:31:56 -0300

    Create idade.js

---
+++ idade.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')({sigint: true});
+var idade, num=0
+
+for idade cont=0; cont<=100,cont++{
+  console.log("Bem Vindo\n")
+  let idade = prompt('Digite sua idade: ')
+  idade = Number(idade)
+
+}
+
+##codigo precisa de correção, falta user input
