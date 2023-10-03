21a550e91c35ebcd0a6efe0260f8e1837a91445b
Emilio Monteiro <monteiroemilio@hotmail.com>
2023-10-03 18:01:16 -0300

    Update index.ts

--- ProjetoFloricultura/index.ts
+++ ProjetoFloricultura/index.ts
@@ -1,105 +1,123 @@
 import express from "express";
-import { Clientes, Produtos, Venda } from "./models";
+import { Client, Product, Sale } from "./models";
+
 const app = express();
 app.use(express.json());
-const clientes: Clientes[] = [];
-const products: Produtos[] = [];
-const vendas: Venda[] = [];
+
+const clients: Client[] = [];
+const products: Product[] = [];
+const sales: Sale[] = [];
+
 app.listen(3000, () => {
-  console.log("Servidor executando na porta 3000");
+  console.log("Server is running on port 3000");
 });
+
 app.get("/", (req, res) => {
   res.send('Floricultura Softex');
 });
-app.post("/clientes", (req, res) => {
-  const client: Clientes = req.body;
-  clientes.push(client);
+
+app.post("/clients", (req, res) => {
+  const client: Client = req.body;
+  clients.push(client);
   res.status(201).json(client);
 });
-app.get("/clientes", (req, res) => {
-  res.status(200).json(clientes);
+
+app.get("/clients", (req, res) => {
+  res.status(200).json(clients);
 });
-app.post("/produtos", (req, res) => {
-  const product: Produtos = req.body;
+
+app.post("/products", (req, res) => {
+  const product: Product = req.body;
   products.push(product);
   res.status(201).json(product);
 });
-app.get("/produtos", (req, res) => {
+
+app.get("/products", (req, res) => {
   res.status(200).json(products);
 });
-app.put("/produtos/:id", (req, res) => {
+
+app.put("/products/:id", (req, res) => {
   const productId = parseInt(req.params.id);
-  const product: Produtos = req.body;
- 
+  const product: Product = req.body;
+
   const index = products.findIndex((p) => p.id === productId);
- 
+
   if (index === -1) {
-     res.status(404).json({ error: "Produto não encontrado" });
+    res.status(404).json({ error: "Product not found" });
   } else {
-     products[index] = product;
-     res.status(200).json(product);
+    products[index] = product;
+    res.status(200).json(product);
   }
- });
- app.delete("/produtos/:id", (req, res) => {
+});
+
+app.delete("/products/:id", (req, res) => {
   const productId = parseInt(req.params.id);
- 
+
   const index = products.findIndex((p) => p.id === productId);
- 
+
   if (index === -1) {
-     res.status(404).json({ error: "Produto não encontrado" });
+    res.status(404).json({ error: "Product not found" });
   } else {
-     products.splice(index, 1);
-     res.status(204).end();
+    products.splice(index, 1);
+    res.status(204).end();
   }
- });
- app.post("/vendas", (req, res) => {
-  const venda: Venda = req.body;
-
-  const clienteExiste = clientes.some(client => client.id == venda.client_id);
-
-  if (!clienteExiste) {
-    return res.status(400).json({ error: "Cliente não cadastrado." });
-  }
-  
-  const precoCorreto = products.some(product => product.price * venda.quantity === venda.total);
-
-  if (!precoCorreto) {
-     return res.status(400).json({ error: "Divergencia de preços." });
-  };
-
-  const produto = products.find(product => product.id === venda.product_id);
-
-  if (!produto) {
-    return res.status(404).json({ error: "Produto não encontrado" });
-  }
-  
-  const stock = produto.estoque;
-  
-  if (venda.quantity > stock) {
-    return res.status(400).json({ error: "Quantidade de produtos vendidos é maior que o estoque disponível." });
-  }
-
-  produto.estoque -= venda.quantity;
-
-  vendas.push(venda);
-  res.status(201).json(venda);
 });
-app.get("/vendas", (req, res) => {
-  res.status(200).json(vendas);
+
+app.post("/sales", (req, res) => {
+  const sale: Sale = req.body;
+
+  const clientExists = clients.some((client) => client.id === sale.clientId);
+
+  if (!clientExists) {
+    return res.status(400).json({ error: "Client not registered." });
+  }
+
+  const correctPrice = products.some(
+    (product) => product.price * sale.quantity === sale.total
+  );
+
+  if (!correctPrice) {
+    return res.status(400).json({ error: "Price discrepancy." });
+  }
+
+  const product = products.find((product) => product.id === sale.productId);
+
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  const stock = product.stock;
+
+  if (sale.quantity > stock) {
+    return res
+      .status(400)
+      .json({ error: "Quantity of sold products exceeds available stock." });
+  }
+
+  product.stock -= sale.quantity;
+
+  sales.push(sale);
+  res.status(201).json(sale);
 });
-app.get("/clientes/:id/vendas", (req, res) => {
-  const clienteId = parseInt(req.params.id);
-  const totalVendas = calcularTotalDoCliente(clienteId);
-  res.status(200).json({ totalVendas });
- });
-function calcularTotalDoCliente(clientId: number): number {
-  let totalVendas = 0;
- 
-  vendas.forEach((venda) => {
-     if (venda.client_id === clientId) {
-       totalVendas += venda.total;
-     }
+
+app.get("/sales", (req, res) => {
+  res.status(200).json(sales);
+});
+
+app.get("/clients/:id/sales", (req, res) => {
+  const clientId = parseInt(req.params.id);
+  const totalSales = calculateTotalSalesForClient(clientId);
+  res.status(200).json({ totalSales });
+});
+
+function calculateTotalSalesForClient(clientId: number): number {
+  let totalSales = 0;
+
+  sales.forEach((sale) => {
+    if (sale.clientId === clientId) {
+      totalSales += sale.total;
+    }
   });
- 
-  return totalVendas;
- }
+
+  return totalSales;
+}
