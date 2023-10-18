6e4d2df576d834b69df967343b8038cf127079a4
Emilio Monteiro <monteiroemilio@hotmail.com>
2023-10-18 16:24:04 -0300

    Create insert chamado.sql

---
+++ mySQL/insert chamado.sql
@@ -0,0 +1,2 @@
+insert into chamado(nome, local, data, dataEncerra, status, descricao, fk_funcionario)
+values ("Reparo torneira", "Hotel Sossego", "2023-10-18", "2023-11-19", "aberto", "Torneira com vazamento", "2")
