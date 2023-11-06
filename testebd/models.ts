554b221310e5a79ddc0fd598794348fde50057b2
Emilio Monteiro <monteiroemilio@hotmail.com>
2023-11-06 12:45:05 -0300

    Create models.ts

---
+++ testebd/models.ts
@@ -0,0 +1,11 @@
+// models.ts
+
+export interface Funcionario {
+  id: number;
+  nome: string;
+  cargo: string;
+  salario: number;
+  dataContratacao: Date;
+}
+
+// Se você precisar de outros tipos de dados, pode defini-los aqui também.
